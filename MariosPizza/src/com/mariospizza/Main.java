3c1df0c09d33ee05a8d1e397004dc9f4d30e6db3
LingDingLauPau <kasperjakob@me.com>
2020-11-02 13:03:11 +0100

    Main description update

--- MariosPizza/src/com/mariospizza/Main.java
+++ MariosPizza/src/com/mariospizza/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// Program that lists a Pizza Menu,
     // can take orders and sort them chronologically based on time.
-    // The system also needs to be able to delete an order once it has been retrieved and payed for.
+    // The system also needs to be able to delete an order once it has been retrieved and paid for.
 
     }
 }
