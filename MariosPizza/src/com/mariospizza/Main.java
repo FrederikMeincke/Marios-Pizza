1ed2aa79ee5dc8b24ca9be42bad847f8e9ebceb3
LingDingLauPau <kasperjakob@me.com>
2020-11-02 15:11:54 +0100

    Pizza class with printPizzaMenu method 1,1

--- MariosPizza/src/com/mariospizza/Main.java
+++ MariosPizza/src/com/mariospizza/Main.java
@@ -15,6 +15,7 @@ public class Main {
     // can take orders and sort them chronologically based on time.
     // The system also needs to be able to delete an order once it has been retrieved and paid for.
 
+        // test print of printPizzaMenu() method
         Pizza.printPizzaMenu();
 
     }
