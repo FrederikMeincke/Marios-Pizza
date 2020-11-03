1a7cac2e81bdc17d91f7cf32db2676814837589b
JimmyJambino <jimmy_losang@hotmail.com>
2020-11-03 14:02:09 +0100

    MyReader fill

--- src/com/mariospizza/MyReader.java
+++ src/com/mariospizza/MyReader.java
@@ -2,12 +2,26 @@ package com.mariospizza;
 
 import java.io.File;
 import java.io.IOException;
+<<<<<<< Updated upstream
+=======
+import java.util.ArrayList;
+>>>>>>> Stashed changes
 import java.util.Scanner;
 
 public class MyReader {
 
-    public void loadPizzaMenu() {
+    public ArrayList<Pizza> loadPizzaMenu() {
+        try {
+            Scanner input = new Scanner(new File("PizzaMenu.txt"));
 
+            while (input.hasNextLine()) {
+                String pizza = input.nextLine();
+                System.out.println(pizza);
+            }
+        } catch (IOException e){
+            System.out.println("File not found.");
+        }
+        return new ArrayList<>(); //TODO: Finish the loading method for the ArrayList
     }
 
     public void loadActiveOrders() {
