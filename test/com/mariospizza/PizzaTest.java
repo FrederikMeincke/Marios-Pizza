070ec975b4df17abe6f662c3b8eccfaa8f1c09e1
LingDingLauPau <kasperjakob@me.com>
2020-11-03 11:16:54 +0100

    Pizza menu test created

--- test/com/mariospizza/PizzaTest.java
+++ test/com/mariospizza/PizzaTest.java
@@ -2,11 +2,19 @@ package com.mariospizza;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PizzaTest {
 
     @Test
     void printPizzaMenu() {
+        File pizzaMenu = new File("PizzaMenu.txt");
+        boolean exists = pizzaMenu.exists();
+        if (exists) {
+            if (pizzaMenu.isFile() && pizzaMenu.exists())
+            System.out.println("Pizza Menu exists");
+        }
     }
 }
