907b4166ab1cee8dc0f87d9c5b6a5d539faf8201
JimmyJambino <jimmy_losang@hotmail.com>
2020-11-03 14:44:51 +0100

    MyReader processLine

--- src/com/mariospizza/FileReader.java
+++ src/com/mariospizza/FileReader.java
@@ -7,6 +7,40 @@ import java.util.Scanner;
 
 public class FileReader {
 
+    public void processPizzaMenuLine(Scanner input) {
+        //int twoDigitsPlusDot = 3; // required length of String for the pizza number including the dot. TODO: Move this to menu printer
+        String pizzaNumber = input.next(); // Includes "." but not spaces.
+        String pizzaName;
+        String pizzaDescription = "";
+        double alm;
+        double deep;
+        double fam;
+
+        /* TODO: Move this to the menu printer
+        if(pizzaNumber.length() < twoDigitsPlusDot) {
+            pizzaNumber += "  "; // Two spaces for formatting
+        } else {
+            pizzaNumber += " "; // One space for formatting
+        }
+         */
+        pizzaName = input.next().substring(-1); // Adds pizzaname and removes ","
+
+        while(!input.hasNextDouble()) {
+            pizzaDescription = input.next(); // Scans through all the toppings on the pizza
+        }
+        //pizzaDescription = pizzaDescription.substring(-1); // Removes "," at the end if necessary
+        alm = input.nextDouble();
+        deep = input.nextDouble();
+        if(deep > 100) { // all family pizzas cost more than 100, so if the next input is more than 100 then we know it's a family pizza and not a deep pan.
+            fam = deep;
+            deep = 0;
+        } else {
+            fam = input.nextDouble(); // in case the price is lower than 100, then the next pizza will be a family pizza.
+        }
+
+        //TODO: Construct pizza object with variables pizzaNumber, pizzaName, alm, deep, fam
+    }
+
     public ArrayList<Pizza> loadPizzaMenu() {
         try {
             Scanner load = new Scanner(new File("PizzaMenu.txt"));
