88dd111bba6250d99ed445f79802d11925f8c49e
LingDingLauPau <kasperjakob@me.com>
2020-11-03 11:00:19 +0100

    test dir created and tests generated

---
+++ test/com/mariospizza/MenuTest.java
@@ -0,0 +1,16 @@
+package com.mariospizza;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    @Test
+    void printMenu() {
+    }
+
+    @Test
+    void readChoice() {
+    }
+}
---
+++ test/com/mariospizza/PizzaTest.java
@@ -0,0 +1,12 @@
+package com.mariospizza;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PizzaTest {
+
+    @Test
+    void printPizzaMenu() {
+    }
+}
