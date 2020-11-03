56fc01deeff6c2939cf7b2b99aa60311885bc5b3
LingDingLauPau <kasperjakob@me.com>
2020-11-03 15:55:53 +0100

    Main.class updated

--- src/com/mariospizza/Main.java
+++ src/com/mariospizza/Main.java
@@ -12,11 +12,6 @@ public class Main {
 	// Program that lists a Pizza Menu,
     // can take orders and sort them chronologically based on time.
     // The system also needs to be able to delete an order once it has been retrieved and paid for.
-
-        // test print of printPizzaMenu() method
-        Pizza.printPizzaMenu();
-        //FileReader.loadCompletedOrders();
-
         Menus.mainMenu();
 
     }
