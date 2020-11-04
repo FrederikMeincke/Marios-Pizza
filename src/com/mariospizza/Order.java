2493ae5c30de2a2a9d178cea78f46dcac67d3842
madsWesth <72575386+madsWesth@users.noreply.github.com>
2020-11-04 14:40:05 +0100

    Order class update

--- src/com/mariospizza/Order.java
+++ src/com/mariospizza/Order.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 
 public class Order {
     Pizza pizza;
-    ArrayList<Pizza> activeOrder = new ArrayList<>();
+    ArrayList<Pizza> orderPizzaList = new ArrayList<>();
+    ArrayList<Order> activeOrdersList = new ArrayList<>();
 
 
-    public Order() {
-
+    public Order(Pizza pizza, ArrayList<Pizza> orderPizzaList, ArrayList<Order> activeOrdersList) {
+        this.pizza = pizza;
+        this.orderPizzaList = orderPizzaList;
+        this.activeOrdersList = activeOrdersList;
     }
 
-    public void addOrder() {
-
+    public void addOrder(Pizza orderPizza) {
+        orderPizzaList.add(orderPizza);
     }
 
     public void changeOrder() {
@@ -25,6 +28,34 @@ public class Order {
 
     public void checkActiveOrders() {
 
+    }
+
+    public void completeOrder() {
+
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public ArrayList<Pizza> getOrderPizzaList() {
+        return orderPizzaList;
+    }
+
+    public void setOrderPizzaList(ArrayList<Pizza> orderPizzaList) {
+        this.orderPizzaList = orderPizzaList;
+    }
+
+    public ArrayList<Order> getActiveOrdersList() {
+        return activeOrdersList;
+    }
+
+    public void setActiveOrdersList(ArrayList<Order> activeOrdersList) {
+        this.activeOrdersList = activeOrdersList;
     }
 
 }
