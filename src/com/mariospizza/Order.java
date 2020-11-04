af0278a9227d73d3435a61132310feca72c08f54
madsWesth <72575386+madsWesth@users.noreply.github.com>
2020-11-04 14:03:09 +0100

    Order class boilerplate

---
+++ src/com/mariospizza/Order.java
@@ -0,0 +1,30 @@
+package com.mariospizza;
+
+import java.util.ArrayList;
+
+public class Order {
+    Pizza pizza;
+    ArrayList<Pizza> activeOrder = new ArrayList<>();
+
+
+    public Order() {
+
+    }
+
+    public void addOrder() {
+
+    }
+
+    public void changeOrder() {
+
+    }
+
+    public void cancelOrder() {
+
+    }
+
+    public void checkActiveOrders() {
+
+    }
+
+}
