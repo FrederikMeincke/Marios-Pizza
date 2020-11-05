215c8bedd8fedee4e9a12505dc9e85999e5bb81f
madsWesth <72575386+madsWesth@users.noreply.github.com>
2020-11-05 12:36:49 +0100

    Order class update. Added ChangeOrder method and added javadoc

--- src/com/mariospizza/Order.java
+++ src/com/mariospizza/Order.java
@@ -40,12 +40,45 @@ public class Order {
         orderPizzaList.add(orderPizza);
     }
 
-    public void changeOrder() {
 
+    public void changeOrder(int orderID) {
+        int whatToChange = -1; // int input by user to indicate what they want changed. Initialized as -1 for compiler
+        //Order is reference type. so if i make a variable of it and change that variable they will both change?
+        Order orderToChange = activeOrdersList.get(orderID);
+        //TODO: menu here for what needs to be changed about the order
+
+        String menuHeader = "CHANGE YOUR ORDER";
+        String leadText = "choose option";
+        String[] menuItems = {"option 1", "option 2", "option 3", "option 4", "option 5"};
+        GenericMenu orderChangeMenu = new GenericMenu(menuHeader, leadText, menuItems);
+
+        //prints the menu and uses user choice to select an option for switch
+        orderChangeMenu.printMenu();
+        switch (orderChangeMenu.readChoice()) {
+            case 1:
+                //System.out.println("add some extra salami");
+                break;
+            case 2:
+                //System.out.println("make deep pan");
+                break;
+            case 3:
+                //System.out.println("i want an extra pizza");
+                break;
+            case 4:
+                //System.out.println("add some extra extra salami");
+                break;
+            case 5:
+                //System.out.println("I want some disgusting pineapple on my pizza. Yikes cringe bro");
+                break;
+            default:
+                System.out.println("invalid option");
+                break;
+        }
     }
 
     /**
      * Deletes order with orderNumber from the active order list
+     *
      * @param orderNumber
      */
     public void cancelOrder(int orderNumber) {
@@ -64,13 +97,23 @@ public class Order {
         for (Order order: activeOrdersList) {
             order.toString();
         }
-
     }
 
+    /**
+     * puts the order in the completed orders list and then removes it from the active orders list
+     *
+     * @param orderToComplete
+     */
     public void completeOrder(Order orderToComplete) {
         completedOrders.add(orderToComplete);
+        activeOrdersList.remove(orderToComplete);
     }
 
+    /**
+     * Deletes an active order
+     *
+     * @param order
+     */
     public void deleteOrder(Order order) {
         activeOrdersList.remove(order);
     }
