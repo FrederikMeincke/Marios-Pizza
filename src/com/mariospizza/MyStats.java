a85f5ff18c4c047a1c8da2694a94c1e460364027
phillip100000 <73539597+phillip100000@users.noreply.github.com>
2020-11-05 12:55:30 +0100

    1. Udkast til pseudo for vores Mystat klasse

--- src/com/mariospizza/MyStats.java
+++ src/com/mariospizza/MyStats.java
@@ -1,4 +1,42 @@
 package com.mariospizza;
 
 public class MyStats {
+
+    // Variables
+/*
+Private int pizzaSoldToday;
+Private int pizzaSoldThisWeek;
+Private int pizzaSoldInAMonth;
+Private int pizzaSoldThisYear;
+Private int allThePizzaSoldForNow;
+int allMyPizzas = pizzaSoldToday;
+ public Pizza getPizza() {
+        return allMyPizzas;
+    }
+    public void set(ArrayList<Order> activeOrdersList) {
+        this.activeOrdersList = activeOrdersList;
+    }
+    int
+public void setActiveOrdersList(ArrayList<Order> activeOrdersList) {
+        this.activeOrdersList = activeOrdersList;
+    }
+
+
+
+Metoder til at udregne mest pop pizza
+Jeg skal vel have en getter. Getteren til at hente data som kan bruges til at lave statestik.
+
+
+ public static void stat1(String[] args) {
+
+
+
+
+
+
+
+
+
+     */
+
 }
