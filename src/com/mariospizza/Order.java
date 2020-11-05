f93fafc1a4d01fe64600090bba41dd8d9a728b83
madsWesth <72575386+madsWesth@users.noreply.github.com>
2020-11-05 13:41:04 +0100

    removed some getters and setters in Order class

--- src/com/mariospizza/Order.java
+++ src/com/mariospizza/Order.java
@@ -118,28 +118,12 @@ public class Order {
         activeOrdersList.remove(order);
     }
 
-    public Pizza getPizza() {
-        return pizza;
-    }
-
-    public void setPizza(Pizza pizza) {
-        this.pizza = pizza;
-    }
-
     public ArrayList<Pizza> getOrderPizzaList() {
         return orderPizzaList;
     }
 
     public void setOrderPizzaList(ArrayList<Pizza> orderPizzaList) {
         this.orderPizzaList = orderPizzaList;
-    }
-
-    public ArrayList<Order> getActiveOrdersList() {
-        return activeOrdersList;
-    }
-
-    public void setActiveOrdersList(ArrayList<Order> activeOrdersList) {
-        this.activeOrdersList = activeOrdersList;
     }
 
     public int getOrderID() {
