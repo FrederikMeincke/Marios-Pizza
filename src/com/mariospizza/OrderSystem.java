9537f8842f309ec228107b6c184a067b96131af8
FrederikMeincke <meincke.frederik@gmail.com>
2020-11-05 13:40:16 +0100

    OrderSystem update - aka Kasper holder pause derfor har vi ikke mange commits, men det er ok fordi han arbejdet stærkt på sine unittests.
    Men ærligt er jeg skuffet vi ikke har set 67 commits i et parge dage.

--- src/com/mariospizza/OrderSystem.java
+++ src/com/mariospizza/OrderSystem.java
@@ -1,5 +1,30 @@
 package com.mariospizza;
 
-public class OrderSystem {
+import java.util.ArrayList;
 
+public class OrderSystem {
+    Pizza pizza;
+
+    ArrayList<Order> activeOrderList = new ArrayList<>();
+    ArrayList<Order> completedOrderList = new ArrayList<>();
+
+    public OrderSystem(ArrayList<Order> activeOrdersList, ArrayList<Order> completedOrderList) {
+        this.activeOrderList = activeOrderList;
+        this.completedOrderList = completedOrderList;
+
+    }
+
+    public void completeOrder(Order orderToComplete) {
+        completedOrderList.add(orderToComplete);
+        activeOrderList.remove(orderToComplete);
+    }
+
+    public void checkActiveOrders() {
+        for (Order order: activeOrderList) {
+            order.toString();
+        }
+    }
+    public void deleteOrder(Order order) {
+        activeOrderList.remove(order);
+    }
 }
