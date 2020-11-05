e1890b3bf157e8b73c9bc085f779c83157410281
phillip100000 <73539597+phillip100000@users.noreply.github.com>
2020-11-05 14:24:51 +0100

    4. Udkast til pseudo for vores Mystat klasse

--- src/com/mariospizza/MyStats.java
+++ src/com/mariospizza/MyStats.java
@@ -4,26 +4,35 @@ public class MyStats {
 
     // Variables
 /*
-Pseudo 0.1
+Metoder til at udregne mest pop pizza
+Jeg skal vel have en getter. Getteren til at hente data som kan bruges til at lave statestik.
 
+
+Pseudo 0.2
+//Variables for overall Pizzas sold
 Private int pizzaSoldToday;
 Private int pizzaSoldThisWeek;
 Private int pizzaSoldInAMonth;
 Private int pizzaSoldThisYear;
 Private int allThePizzaSoldForNow;
+// Variables for the most popular pizzas
 Private string mostPopularPizza1;
 Private string mostPopularPizza2;
 Private string mostPopularPizza3;
 Private String mostPopularPizza4;
+// Temporarily variables
 int allMyPizzasForToday;
 int allMyPizzasThisWeek;
 int allMyPizzasThisMonth;
+int allThePizzas;
+// Equation for setting the private ints
 pizzaSoldToday = allMyPizzasForToday;
 pizzaSoldThisWeek = allMyPizzasThisWeek;
 pizzaSoldInAMonth = allMyPizzasThisMonth;
 pizzaSoldThisYear = allMyPizzasThisYear;
 allThePizzaSoldForNow = allThePizzas;
 
+// Getters for temporaries
  public Pizza getPizzaToday() {
         return allMyPizzasForToday;
     }
@@ -39,15 +48,10 @@ allThePizzaSoldForNow = allThePizzas;
  public Pizza getAllOfThePizzas() {
         return allThePizzas;
     }
+//Getters for temporaries
 
-
-Metoder til at udregne mest pop pizza
-Jeg skal vel have en getter. Getteren til at hente data som kan bruges til at lave statestik.
-
-
- public static void stat1(String[] args) {
-
-
+ public Pizza getAllOfThePizzas() {
+        return allThePizzas;
 
 
 
