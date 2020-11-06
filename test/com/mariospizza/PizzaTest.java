4372ea0e8d92513b2a444ccc139d2c1b6dbd68fc
JimmyJambino <jimmy_losang@hotmail.com>
2020-11-06 12:12:02 +0100

    added some automatic tests

--- test/com/mariospizza/PizzaTest.java
+++ test/com/mariospizza/PizzaTest.java
@@ -8,28 +8,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PizzaTest {
 
+    Pizza pizza = new Pizza(1,"pizzaTest","boloni,sauerkraut,tears",35.0,55.0,105.0);
+
     @Test
     void testPrintPizzaMenu() {
     }
 
     @Test
     void setPizzaName() {
+        String nameTest = "newName";
+        pizza.setPizzaName(nameTest);
+        assertEquals(nameTest,pizza.getPizzaName());
     }
 
     @Test
     void setPizzaDescription() {
+        String descriptionTest = "newDescription, moreDescription";
+        pizza.setPizzaDescription(descriptionTest);
+        assertEquals(descriptionTest,pizza.getPizzaDescription());
     }
 
     @Test
     void setPriceNormal() {
+        double priceTest = 50.0;
+        pizza.setPriceNormal(priceTest);
+        assertEquals(priceTest,pizza.getPriceNormal());
     }
 
     @Test
     void setPriceDeep() {
+        double priceTest = 75.0;
+        pizza.setPriceDeep(priceTest);
+        assertEquals(priceTest,pizza.getPriceDeep());
     }
 
     @Test
     void setPriceFamily() {
+        double priceTest = 110.0;
+        pizza.setPriceFamily(priceTest);
+        assertEquals(priceTest,pizza.getPriceFamily());
     }
 
     @Test
