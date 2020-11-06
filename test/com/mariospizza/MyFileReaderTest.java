3f9049e686404668e4c024cce6879d2be04e6b6b
JimmyJambino <jimmy_losang@hotmail.com>
2020-11-06 14:15:20 +0100

    cleaned up in folder

--- test/com/mariospizza/MyFileReaderTest.java
+++ test/com/mariospizza/MyFileReaderTest.java
@@ -2,9 +2,13 @@ package com.mariospizza;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyFileReaderTest {
+
+    MyFileReader testFileReader = new MyFileReader();
 
     @Test
     void processPizzaMenuLine() {
@@ -12,6 +16,22 @@ class MyFileReaderTest {
 
     @Test
     void loadPizzaMenu() {
+        //Loading our pizzaMenu text file into an ArrayList<Pizza>
+        //If it loads correctly, we expect several Pizza objects inside the ArrayList<Pizza>
+        //and Each Pizza object will have its variables assigned from the text file.
+        //Our headers are also Pizza objects but don't contain important information besides their name
+        /* test.txt
+        testHeader
+        5. pizzaNameTest1, description1, description2, 40 70 101
+        3. pizzaNameTest2, description3, description4, 50 80 102
+        testHeader2
+        6. pizzaNameTest3, description5, 60 90 120
+         */
+        ArrayList<Pizza> testPizzaList = testFileReader.loadPizzaMenu("TestFile");
+        //System.out.println(testPizzaList.get(0).getPizzaID());
+        //System.out.println(testPizzaList.get(0).getPizzaName());
+        //assertEquals("testHeader",testPizzaList.get(0).getPizzaName());
+
     }
 
     @Test
