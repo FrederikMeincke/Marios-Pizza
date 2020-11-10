c94d84c1fd794ba0c53a4a8060fb73e9ab30196c
JimmyJambino <jimmy_losang@hotmail.com>
2020-11-10 14:23:26 +0100

    minus junk

--- src/com/mariospizza/MyFileWriter.java
+++ src/com/mariospizza/MyFileWriter.java
@@ -7,25 +7,6 @@ import java.util.ArrayList;
 
 public class MyFileWriter {
 
-    // TODO: write to files
-
-    /**
-     * Save completed orders in an arraylist and file.txt
-     * @param orderList
-     * @param fileName
-     */
-    public void saveCompletedOrders(ArrayList<Order> orderList, String fileName) {
-        try {
-            PrintStream output = new PrintStream(new File(fileName+".txt"));
-            for (int i = 0; i < orderList.size(); i++) {
-                output.println(orderList.get(i).toString()); // TODO: Add toString in Order that prints the information we need in the text file
-            }
-        } catch (IOException e) {
-            //TODO: Add message
-        }
-
-    }
-
     /**
      * Saved active orders in an arraylist and file.txt
      * @param orderList
