5334163f42ef80e701b61f101a4a64f7edf29ac6
JimmyJambino <jimmy_losang@hotmail.com>
2020-11-10 14:22:36 +0100

    removed more junk

--- src/com/mariospizza/MyFileWriter.java
+++ src/com/mariospizza/MyFileWriter.java
@@ -32,7 +32,6 @@ public class MyFileWriter {
      * @param fileName
      */
     public void updateOrderFile(ArrayList<Order> orderList, String fileName) {
-        //TODO: MAKE A NEW LINE AT THE BEGINNING OF FILE, NOT AT THE END! AND FIRST FILE WON'T MAKE A LINE ABOVE IT
         try {
             PrintStream output = new PrintStream(new File(fileName+".txt"));
             for(int i = 0; i < orderList.size(); i++) {
@@ -45,36 +44,6 @@ public class MyFileWriter {
                 }
                 output.println(orderIDString + " " + pizzaList);
             }
-/*
-            for (int i = 0; i < orderList.size(); i++) {
-                if(i+1 != orderList.size()) { // Explain why i+1
-                    int orderID = orderList.get(i).getOrderID();
-                    String orderIDString = String.format("%d",orderID);
-                    String pizzaList = "";
-                    for(int j = 0; j < orderList.get(i).getOrderPizzaList().size(); j++) {
-                        if(j+1 != orderList.get(i).getOrderPizzaList().size()) {
-                            pizzaList += orderList.get(i).getOrderPizzaList().get(j).pizzaID + " " +
-                                    + orderList.get(i).getOrderPizzaList().get(j).getPizzaSize() + " ";
-                        } else {
-                            pizzaList += orderList.get(i).getOrderPizzaList().get(j).pizzaID + " " +
-                                    + orderList.get(i).getOrderPizzaList().get(j).getPizzaSize() + " ";
-                        }
-
-                    }
-                    output.println(orderIDString + " " + pizzaList);
-                } else { // Prevents an empty line for the last order in the text file
-                    int orderID = orderList.get(i).getOrderID();
-                    String orderIDString = String.format("%d",orderID);
-                    String pizzaList = "";
-                    for(int j = 0; j < orderList.get(i).getOrderPizzaList().size(); j++) {
-                        pizzaList += orderList.get(i).getOrderPizzaList().get(j).pizzaID + " " +
-                                orderList.get(i).getOrderPizzaList().get(j).getPizzaSize();
-                    }
-                    output.print(orderIDString + " " + pizzaList);
-                }
-            }
-
- */
         } catch (IOException e) {
             //TODO: Add message
         }
