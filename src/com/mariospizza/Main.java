5b44be5e94927cae6a21a9f90651508d6dbdb6b4
FrederikMeincke <meincke.frederik@gmail.com>
2020-11-11 12:34:09 +0100

    updated @Version 0.7

--- src/com/mariospizza/Main.java
+++ src/com/mariospizza/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * Marios Pizza Menu POS for command line application
  * @author Frederik M., Jimmy, Kasper, Mads, Philip
- * @version 0.7
+ * @version 0.8
  */
 
 public class Main {
